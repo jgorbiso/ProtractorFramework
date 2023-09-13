0dabe1229a2e3b88ba36ae17a57f2db7cf9f20d8
jgorbiso <jgorbiso@gmail.com>
2023-09-13 15:09:21 +0800

    Removed Error generating update to show successful build

--- tests/calculator.js
+++ tests/calculator.js
@@ -17,19 +17,19 @@ describe("demo calculator tests", function () {
         browser.sleep(2000);
     });
 
-    it("subtraction test", function () {
-        // browser.get('https://juliemr.github.io/protractor-demo/');
-        homepage.get('https://juliemr.github.io/protractor-demo/');
-        // element(by.model('first')).sendKeys("2");
-        homepage.enterFirstNumber('2');
-        // element(by.model('second')).sendKeys("3");
-        homepage.enterSecondNumber('3');
-        // element(by.css('[ng-click="doAddition()"]')).click();
-        homepage.clickGoButtonError();
-        // let result = element(by.cssContainingText(".ng-binding", "5"));
-        // expect(result.getText()).toEqual("5");
-        homepage.verifyResult('5');
+    // it("subtraction test", function () {
+    //     // browser.get('https://juliemr.github.io/protractor-demo/');
+    //     homepage.get('https://juliemr.github.io/protractor-demo/');
+    //     // element(by.model('first')).sendKeys("2");
+    //     homepage.enterFirstNumber('2');
+    //     // element(by.model('second')).sendKeys("3");
+    //     homepage.enterSecondNumber('3');
+    //     // element(by.css('[ng-click="doAddition()"]')).click();
+    //     homepage.clickGoButtonError();
+    //     // let result = element(by.cssContainingText(".ng-binding", "5"));
+    //     // expect(result.getText()).toEqual("5");
+    //     homepage.verifyResult('5');
 
-        browser.sleep(3000);
-    });
+    //     browser.sleep(3000);
+    // });
 });
